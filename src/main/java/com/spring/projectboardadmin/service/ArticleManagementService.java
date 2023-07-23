65fc522f43d2416366772ed21848a7be79e1e3a9
JadeKim042386 <jk042386@gmail.com>
2023-07-23 21:40:34 +0900

    feat: 게시글 관리 페이지 구현 - 게시글 관리 페이지 컨트롤러, 서비스, 뷰 구현 #31

--- src/main/java/com/spring/projectboardadmin/service/ArticleManagementService.java
+++ src/main/java/com/spring/projectboardadmin/service/ArticleManagementService.java
@@ -1,21 +1,62 @@
 package com.spring.projectboardadmin.service;
 
 import com.spring.projectboardadmin.dto.ArticleDto;
+import com.spring.projectboardadmin.dto.properties.ProjectProperties;
+import com.spring.projectboardadmin.dto.response.ArticleClientResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
 public class ArticleManagementService {
-    public List<ArticleDto> getArticles() {
-        return List.of();
-    }
-    public ArticleDto getArticle(int articleIndex, int pageNumber) {
-        return null;
-    }
-    public void deleteArticle(Long articleId) {
+    private final RestTemplate restTemplate;
+    private final ProjectProperties projectProperties;
 
+    public List<ArticleDto> getArticles() {
+        URI uri = UriComponentsBuilder
+                .fromHttpUrl(
+                        projectProperties.board().url()
+                                + "/api/articles"
+                )
+                .queryParam("size", 10000)
+                .build()
+                .toUri();
+        ArticleClientResponse response = restTemplate.getForObject(uri, ArticleClientResponse.class);
+
+        return Optional.ofNullable(response)
+                .orElseGet(ArticleClientResponse::empty).articles();
+    }
+    public ArticleDto getArticle(Long articleId) {
+        URI uri = UriComponentsBuilder.fromHttpUrl(
+                projectProperties.board().url()
+                        + "/api/articles/"
+                        + articleId
+                )
+                .build()
+                .toUri();
+        ArticleDto response = restTemplate.getForObject(uri, ArticleDto.class);
+
+        return Optional.ofNullable(response)
+                .orElseThrow(() -> new NoSuchElementException("게시글이 없습니다 - articleId: " + articleId));
+    }
+
+    public void deleteArticle(Long articleId) {
+        URI uri = UriComponentsBuilder
+                .fromHttpUrl(
+                        projectProperties.board().url()
+                                + "/api/articles/"
+                                + articleId
+                                + "/delete"
+                )
+                .build()
+                .toUri();
+        restTemplate.delete(uri);
     }
 }
